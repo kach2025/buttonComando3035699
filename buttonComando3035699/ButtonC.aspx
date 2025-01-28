3164b1ffa30fb0b0306ea986f674b84ea7aad784
acamp <acamp@KEVINSIN>
2025-01-28 15:16:50 -0600

    Agregar archivos de proyecto.

---
+++ buttonComando3035699/ButtonC.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ButtonC.aspx.cs" Inherits="buttonComando3035699.ButtonC" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Bold="true" Text="View Supplier"></asp:Label><br />
+            <asp:Button runat="server" CommandArgument="Adidas" CommandName="View"
+                OnCommand="Butto_Command" Text="Adidas"/>
+            <asp:Button runat="server" CommandArgument="Babolat" CommandName="View" 
+                OnCommand="Butto_Command" Text="Babolat"/>
+            <asp:Button runat="server" CommandArgument="Head" CommandName="View" 
+                OnCommand="Butto_Command" Text="Head"/>
+            <asp:Button runat="server" CommandArgument="Nike" CommandName="View"
+                 OnCommand="Butto_Command" Text="Nike"/>
+            <asp:Button runat="server" CommandArgument="Prince" CommandName="View"
+                OnCommand="Butto_Command" Text="Prince"/>
+            <asp:Button runat="server" CommandName="Cancel" 
+                OnCommand="Butto_Command" Text="Cancel"/> <br /> <br />
+            <asp:Label ID="lblMessage" runat="server"></asp:Label><br />
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ buttonComando3035699/ButtonC.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace buttonComando3035699
+{
+    public partial class ButtonC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Butto_Command(object sender, CommandEventArgs e)
+        {
+            if (e.CommandName == "View")
+            {
+
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+                switch (e.CommandArgument.ToString())
+                {
+                    case "Adidas":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+
+                    case "Babolat":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+
+                    case "Head":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+
+                    case "Nike":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+
+                    case "Prince":
+                        lblMessage.Text = e.CommandArgument.ToString();
+                        break;
+                }
+
+            }
+            else { 
+            lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Cancel";
+            
+            }
+        }
+    }
+}
